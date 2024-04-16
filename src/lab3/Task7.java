2a599e2e1082f568b624d4604534228a792f005f
Sofia Golovina <sofi-golovina@ya.ru>
2024-04-16 22:57:27 +0500

    Already written examples

--- src/lab3/Task7.java
+++ src/lab3/Task7.java
@@ -1,57 +1,62 @@
 package lab3;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
+// Класс Node берем из Task4
+class LinkedList {
+    Node head;
+    Node tail;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addNodeAtHead(int data) {
+        Node newNode = new Node(data, head);
+        head = newNode;
+        if (tail == null) {
+            tail = newNode;
+        }
+    }
+
+    public void addNodeAtTail(int data) {
+        Node newNode = new Node(data, null);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
 public class Task7 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Сколько человек? ");
-        int N = scanner.nextInt();
+        LinkedList listFromHead = new LinkedList();
+        System.out.println("Список с головы:");
+        listFromHead.addNodeAtHead(1);
+        listFromHead.printList();
+        listFromHead.addNodeAtHead(2);
+        listFromHead.printList();
+        listFromHead.addNodeAtHead(3);
+        listFromHead.printList();
 
-        long startTimeArrayList = System.nanoTime();
-        useArrayList(N);
-        long endTimeArrayList = System.nanoTime();
-        long durationArrayList = (endTimeArrayList - startTimeArrayList) / 1000000; // Время в миллисекундах
-
-        long startTimeLinkedList = System.nanoTime();
-        useLinkedList(N);
-        long endTimeLinkedList = System.nanoTime();
-        long durationLinkedList = (endTimeLinkedList - startTimeLinkedList) / 1000000; // Время в миллисекундах
-
-        System.out.println("Время выполнения метода с ArrayList: " + durationArrayList + " мс");
-        System.out.println("Время выполнения метода с LinkedList: " + durationLinkedList + " мс");
-    }
-
-    public static void useArrayList(int N) {
-        List<Integer> people = new ArrayList<>();
-        for (int i = 1; i <= N; i++) {
-            people.add(i);
-        }
-
-        int index = 0;
-        while (people.size() > 1) {
-            index = (index + 1) % people.size();
-            people.remove(index);
-        }
-
-        System.out.println("Выживший человек (ArrayList): " + people.getFirst());
-    }
-
-    public static void useLinkedList(int N) {
-        List<Integer> people = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
-            people.add(i);
-        }
-
-        int index = 0;
-        while (people.size() > 1) {
-            index = (index + 1) % people.size();
-            people.remove(index);
-        }
-
-        System.out.println("Выживший человек (LinkedList): " + people.getFirst());
+        LinkedList listFromTail = new LinkedList();
+        System.out.println("\nСписок с хвоста:");
+        listFromTail.addNodeAtTail(1);
+        listFromTail.printList();
+        listFromTail.addNodeAtTail(2);
+        listFromTail.printList();
+        listFromTail.addNodeAtTail(3);
+        listFromTail.printList();
     }
 }
